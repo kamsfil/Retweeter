18de83a5729b5a30469feef4ae695ca3d8370940
kamsfil <kamsfil@gmail.com>
2023-03-18 14:47:34 +0300

    1

--- src/main/java/com/example/demo/service/UserSevice.java
+++ src/main/java/com/example/demo/service/UserSevice.java
@@ -53,7 +53,7 @@ public class UserSevice implements UserDetailsService {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
-                            "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                            "Welcome to Retweeter. Please, visit next link: http://localhost:8080/activate/%s",
                     user.getUsername(),
                     user.getActivationCode()
             );
